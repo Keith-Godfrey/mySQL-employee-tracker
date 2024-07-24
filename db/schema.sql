13382f6fa420636e5d9242306074d787346eea46
Keith Godfrey <keith.godfrey23@gmail.com>
2024-07-24 09:43:56 -0700

    fixed errors on server.js and ran tests

--- db/schema.sql
+++ db/schema.sql
@@ -1,29 +1,29 @@
--- Create a employees database
 DROP DATABASE IF EXISTS employees_db;
+-- create database
 CREATE DATABASE employees_db;
 
+-- connects to database
 \c employees_db;
 
--- department table
+-- creates table
 CREATE TABLE department (
-    id SERIAL PRIMARY KEY
-    name: VARCHAR(30) UNIQUE NOT NULL
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(30) NOT NULL
 );
 
--- role table
+-- creates table
 CREATE TABLE role (
-    id: SERIAL PRIMARY KEY
-    title: VARCHAR(30) UNIQUE NOT NULL 
-    salary: DECIMAL NOT NULL 
-    department_id: INTEGER NOT NULL
+  id SERIAL PRIMARY KEY NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INTEGER NOT NULL
 );
 
-
--- employee table
+-- creates table
 CREATE TABLE employee (
-    id: SERIAL PRIMARY KEY
-    first_name: VARCHAR(30) NOT NULL 
-    last_name: VARCHAR(30) NOT NULL 
-    role_id: INTEGER NOT NULL 
-    manager_id: INTEGER 
+  id SERIAL PRIMARY KEY NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER NOT NULL,
+  manager_id INTEGER
 );
